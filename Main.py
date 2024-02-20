ff0447178e7d44d8cd521f2ef8d79be89750d5bc
juankASL33 <98744511+juankASL33@users.noreply.github.com>
2024-02-20 09:56:31 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,20 @@
+from Funciones import *
+print("Bienvenida al menú del libro de recetas.")
+opcion = menu()
+while opcion != 6:
+    if opcion == 1:
+        opcion1(recetas)
+    elif opcion == 2:
+        opcion2(recetas)
+    elif opcion == 3:
+        opcion3(recetas)
+    elif opcion == 4:
+        opcion4(recetas)
+    elif opcion == 5:
+        opcion5(recetas)
+    else:
+        erroropcion()
+    opcion = menu()
+print()
+print("Saliendo del programa. Muchas Gracias")
+print()
